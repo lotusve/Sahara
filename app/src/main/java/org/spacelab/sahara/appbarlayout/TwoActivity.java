32921e685d37c9357db21848ca523a156f218e62
lotusve <lotusve@163.com>
2019-10-17 19:13:33 +0800

    add SwipeRefreshLayout.

--- app/src/main/java/org/spacelab/sahara/appbarlayout/TwoActivity.java
+++ app/src/main/java/org/spacelab/sahara/appbarlayout/TwoActivity.java
@@ -1,10 +1,13 @@
 package org.spacelab.sahara.appbarlayout;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import org.spacelab.sahara.R;
 
@@ -12,6 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TwoActivity extends AppCompatActivity {
+
+    private Context context;
+
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     private RecyclerView recyclerView;
 
@@ -25,19 +32,27 @@ public class TwoActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_two);
 
+        context = this;
+
         initView();
     }
 
     private void initView() {
 
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(context, "refreshing.", Toast.LENGTH_SHORT).show();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+
         recyclerView = findViewById(R.id.recycler_view);
-
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
         adapter = new TwoAdapter(this);
-
         recyclerView.setAdapter(adapter);
-
     }
 
     @Override
